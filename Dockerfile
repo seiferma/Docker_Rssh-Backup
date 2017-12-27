b139b67786dec82429d0a61aca780e6701fd6735
Stephan Seifermann <seiferma@users.noreply.github.com>
2017-12-27 23:25:48 +0100

    Initial commit.

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM alpine:latest
+
+WORKDIR /rssh
+
+RUN RSSH_VERSION=2.3.4-r0 && \
+    RSYNC_VERSION=3.1.2-r6 && \
+    SSH_VERSION=7.5_p1-r8 && \
+    apk --no-cache add rssh=${RSSH_VERSION} rsync=${RSYNC_VERSION} openssh-server=${SSH_VERSION} && \
+    RSSH_VERSION= && \
+    RSYNC_VERSION= && \
+    SSH_VERSION=
+
+ENV VOL_HOME=/rssh/home
+ENV VOL_CFG=/rssh/cfg
+
+VOLUME ["${VOL_HOME}", "${VOL_CFG}"]
+
+EXPOSE 22
+
+COPY ["init.sh", "./"]
+COPY ["sshd_config", "/etc/ssh/"]
+COPY ["rssh.conf", "/etc"]
+
+ENTRYPOINT ["./init.sh"]
+CMD ["sshd"]
---
+++ init.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+set -e
+
+UNIX_USERS=$VOL_CFG/users
+SSH_KEYS=$VOL_CFG/keys
+HOMES=$VOL_HOME
+
+if [ ! -f $UNIX_USERS/passwd ] || [ ! -f $UNIX_USERS/group ] || [ ! -f $UNIX_USERS/shadow ]; then
+    echo "Initializing unix users in volume"
+    mkdir -p $UNIX_USERS
+    cp -f /etc/passwd $UNIX_USERS/
+    cp -f /etc/group $UNIX_USERS/
+    cp -f /etc/shadow $UNIX_USERS/
+fi
+
+echo "Using unix users from volume"
+rm /etc/passwd
+ln -s $UNIX_USERS/passwd /etc/passwd
+rm /etc/group
+ln -s $UNIX_USERS/group /etc/group
+rm /etc/shadow
+ln -s $UNIX_USERS/shadow /etc/shadow
+
+if [ ! -f $SSH_KEYS/ssh_host_ed25519_key ] || [ ! -f $SSH_KEYS/ssh_host_rsa_key ]; then
+    echo "Initializing sshd keys in volume"
+    rm -f $SSH_KEYS
+    mkdir -p $SSH_KEYS
+    ssh-keygen -q -N "" -t ed25519 -f $SSH_KEYS/ssh_host_ed25519_key
+    ssh-keygen -q -N "" -t rsa -f $SSH_KEYS/ssh_host_rsa_key
+    chmod 600 $SSH_KEYS/*
+fi
+
+echo "Using sshd keys from volume"
+rm -f /etc/ssh/ssh_host_*
+ln -s $SSH_KEYS/ssh_host_rsa_key /etc/ssh/ssh_host_rsa_key
+ln -s $SSH_KEYS/ssh_host_ed25519_key /etc/ssh/ssh_host_ed25519_key
+
+if [ "$1" == "sshd" ]; then
+    echo "Starting ssh server"
+    /usr/sbin/sshd -D -e
+
+elif [ "$1" == "adduser" ]; then
+    if [ -z "$2" ] || [ -z "$3" ]; then
+        echo "You have to specify an username and a public key"
+        exit 1
+    fi
+    echo "Adding new user $2"
+    USERDIR=$HOMES/$2
+    adduser -s $(which rssh) -h $USERDIR -D $2
+    mkdir $USERDIR/.ssh
+    chmod -R 750 $USERDIR
+    echo $3 > $USERDIR/.ssh/authorized_keys
+
+else
+    echo "Executing command"
+    exec "$@"
+fi
